c5fcc30383eede42e4e780dc18612da422239880
Quynh Chi Lai <quynhchi1009@yandex.com>
2023-04-25 14:27:09 +0200

    Async post, get, delete Data

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
     todosSelector,
     markComplete,
     deleteTodo,
+    getTodos
 } from "../store/reducers/todosSlice";
 import TodoForm from "./Todoform";
 
@@ -11,28 +12,39 @@ const Todos = () => {
     const todos = useSelector(todosSelector);
     const dispatch = useDispatch();
 
-    const toggleTodoCompleted = todoId => {
+    const toggleTodoCompleted = (todoId) => {
         // console.log(todoId);
-        dispatch(markComplete(todoId))
-    }
+        dispatch(markComplete(todoId));
+    };
 
-    const deleteSingleTodo = todoId => {
-        dispatch(deleteTodo(todoId))
-    }
+    const deleteSingleTodo = (todoId) => {
+        dispatch(deleteTodo(todoId));
+    };
+
+    //[], Component chi chay 1 lan luc khoi tao, khong chay khi thay doi
+    useEffect(() => {
+        //Send request to JSONplaceholder
+        dispatch(getTodos());
+        //dispatch
+    }, [dispatch]);
 
     return (
         <div className='todo-list'>
             <TodoForm />
             <ul>
                 {todos.map((todo) => (
-                    <li key={todo.id} className={todo.completed ? 'completed' : ''}>
+                    <li
+                        key={todo.id}
+                        className={todo.completed ? "completed" : ""}>
                         {todo.title}
                         <input
                             type='checkbox'
                             checked={todo.completed}
                             onChange={toggleTodoCompleted.bind(this, todo.id)}
                         />
-                        <button onClick={deleteSingleTodo.bind(this, todo.id)}>Delete</button>
+                        <button onClick={deleteSingleTodo.bind(this, todo.id)}>
+                            Delete
+                        </button>
                     </li>
                 ))}
             </ul>
--- src/store/reducers/todosSlice.js
+++ src/store/reducers/todosSlice.js
@@ -1,29 +1,40 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, nanoid } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Reducer Thunk
+export const getTodos = createAsyncThunk("todos/todosFetched", async () => {
+    const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10"
+    );
+    return response.data;
+});
+
+export const addTodo = createAsyncThunk("todos/todoAdded", async (title) => {
+    const newTodo = {
+        id: nanoid(),
+        title,
+        completed: false,
+    };
+    await axios.post("https://jsonplaceholder.typicode.com/todos", newTodo);
+    return newTodo;
+});
+
+export const deleteTodo = createAsyncThunk(
+    "todos/todoDeleted",
+    async (todoId) => {
+        await axios.delete(
+            `https://jsonplaceholder.typicode.com/todos/${todoId}`
+        );
+        return todoId;
+    }
+);
 
 const todosSlice = createSlice({
     name: "todos",
     initialState: {
-        allTodos: [
-            { id: 1, title: "Sign Paper", completed: false },
-            { id: 2, title: "Read Paper", completed: false },
-            { id: 3, title: "Packet", completed: true },
-        ],
+        allTodos: [],
     },
     reducers: {
-        addTodo: {
-            reducer(state, action) {
-                state.allTodos.unshift(action.payload);
-            },
-            prepare(title) {
-                return {
-                    payload: {
-                        id: nanoid(),
-                        title,
-                        completed: false,
-                    },
-                };
-            },
-        },
         markComplete(state, action) {
             const todoId = action.payload;
             state.allTodos = state.allTodos.map((todo) => {
@@ -31,10 +42,32 @@ const todosSlice = createSlice({
                 return todo;
             });
         },
-        deleteTodo(state, action) {
-            const todoId = action.payload
-            state.allTodos = state.allTodos.filter(todo => todo.id !== todoId)
-        }
+    },
+    extraReducers: {
+        //Get All todos
+        [getTodos.pending]: (state, action) => {
+            console.log("Fetching todos from Backend....");
+        },
+        [getTodos.fulfilled]: (state, action) => {
+            console.log("Fetched!");
+            state.allTodos = action.payload;
+        },
+        [getTodos.rejected]: (state, action) => {
+            console.log("Failed fetching..");
+        },
+
+        //Add todo
+        [addTodo.fulfilled]: (state, action) => {
+            state.allTodos.unshift(action.payload);
+        },
+
+        //Delete todo
+        [deleteTodo.fulfilled]: (state, action) => {
+            const todoId = action.payload;
+            state.allTodos = state.allTodos.filter(
+                (todo) => todo.id !== todoId
+            );
+        },
     },
 });
 
@@ -45,7 +78,7 @@ const todosReducer = todosSlice.reducer;
 export const todosSelector = (state) => state.todosReducer.allTodos;
 
 //Export Action
-export const { addTodo, markComplete, deleteTodo } = todosSlice.actions;
+export const { markComplete } = todosSlice.actions;
 
 //Export Reducer
 export default todosReducer;
